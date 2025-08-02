76684ff1c4a65907c69a3642e80e27c39ea22263
Dhvani <118267279+ChavdaDhvani@users.noreply.github.com>
2025-08-02 20:25:21 +0530

    Update SearchBox.jsx

--- Weather-react-app/src/SearchBox.jsx
+++ Weather-react-app/src/SearchBox.jsx
@@ -9,7 +9,7 @@ export default function SearchBox({updateInfo}){
     let [city, setCity] = useState("");
 
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = Add your key;
+    const API_KEY = Add_your_key;
 
     let getWeatherInfo = async () =>{
      let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
